b4693623a9ab2e21ed52f45cc0b0c4e5a3a02008
zeoly <zeoly100@163.com>
2019-12-30 14:18:32 +0800

    css

--- src/main/java/com/yahacode/ticket/fx/TicketFxApplication.java
+++ src/main/java/com/yahacode/ticket/fx/TicketFxApplication.java
@@ -12,9 +12,6 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -26,13 +23,14 @@ public class TicketFxApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         GridPane grid = new GridPane();
+        grid.setGridLinesVisible(true);
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
         Text sceneTitle = new Text("Welcome");
-        sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        sceneTitle.setId("welcome-text");
         grid.add(sceneTitle, 0, 0, 2, 1);
 
         Label userName = new Label("User Name:");
@@ -54,12 +52,12 @@ public class TicketFxApplication extends Application {
         grid.add(hbBtn, 1, 4);
 
         final Text actionTarget = new Text();
+        actionTarget.setId("action-target");
         grid.add(actionTarget, 1, 6);
 
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
-                actionTarget.setFill(Color.FIREBRICK);
                 actionTarget.setText("Sign in button pressed");
             }
         });
@@ -67,6 +65,9 @@ public class TicketFxApplication extends Application {
         Scene scene = new Scene(grid, 300, 275);
         primaryStage.setTitle("Ticket App Beta");
         primaryStage.setScene(scene);
+
+        scene.getStylesheets().add("test.css");
+
         primaryStage.show();
     }
 }
